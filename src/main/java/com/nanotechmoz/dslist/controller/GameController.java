def6b819d72498c5bb77dd053f76fdb5b788e247
JanuarioAznick <januario-jose@outlook.com>
2023-07-20 17:22:46 +0200

    Get game by id

--- src/main/java/com/nanotechmoz/dslist/controller/GameController.java
+++ src/main/java/com/nanotechmoz/dslist/controller/GameController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nanotechmoz.dslist.dto.GameDTO;
 import com.nanotechmoz.dslist.dto.GameMinDTO;
+import com.nanotechmoz.dslist.entities.Game;
 import com.nanotechmoz.dslist.services.GameService;
 
 @RestController
@@ -16,6 +19,13 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO game = gameService.findById(id);
+		return game;
+		
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
--- src/main/java/com/nanotechmoz/dslist/services/GameService.java
+++ src/main/java/com/nanotechmoz/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nanotechmoz.dslist.dto.GameDTO;
 import com.nanotechmoz.dslist.dto.GameMinDTO;
 import com.nanotechmoz.dslist.entities.Game;
 import com.nanotechmoz.dslist.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		return new GameDTO(game);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> game = gameRepository.findAll();
 		List<GameMinDTO> dto = game.stream().map(x -> new GameMinDTO(x)).toList();
