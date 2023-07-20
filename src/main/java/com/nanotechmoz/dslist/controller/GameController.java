c4b3e51afeb143489b2735f82a950d9a62c75dd4
JanuarioAznick <januario-jose@outlook.com>
2023-07-20 15:04:50 +0200

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/nanotechmoz/dslist/controller/GameController.java
@@ -0,0 +1,27 @@
+package com.nanotechmoz.dslist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nanotechmoz.dslist.dto.GameMinDTO;
+import com.nanotechmoz.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> game = gameService.findAll();
+		return game;
+		
+	}
+
+}
